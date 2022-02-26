86620c602d14992d30c8fc2f290abc044419ec12
Faraaz2497 <76690389+Faraaz2497@users.noreply.github.com>
2022-02-26 18:05:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 link="https://teachablemachine.withgoogle.com/models/9_s5Y9eB-/model.json";
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier(link,modelReady);
+}
+function modelReady(){
+    classifier.classify(getResult);
+}
+function getResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_label").innerHTML="I can hear:"+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy:"+(results[0].confidence*100).toFixed(2)+"%";
+
+        img1=document.getElementById("image1");
+        
+        if(results[0].label=="Cat"){
+            img1.src="Cat.gif";
+        }
+        else if(results[0].label=="Dog"){
+            img1.src="Dog.gif";
+        }
+        
+    }
+}
+
